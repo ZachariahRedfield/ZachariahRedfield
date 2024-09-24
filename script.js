38399e7f985c12d1afef1aad61ba22790452f7e7
ZachariahRedfield <zjhredfield@icloud.com>
2024-09-24 13:13:03 -0500

    Fixed an error in styles.css | Added title to label for toggle box | Fixed formatting

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const themeLabel = document.getElementById('theme-label');
 
 toggleCheckbox.addEventListener('change', () => {
     document.body.classList.toggle('dark-mode', toggleCheckbox.checked);
-    
+
     // Change the label to indicate the current theme
     themeLabel.textContent = toggleCheckbox.checked ? '🌞' : '🌙'; // Change to sun emoji when in dark mode
 });
