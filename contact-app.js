737e7fe690d85819712d4ce41180ba9ba6aeb435
ZachariahRedfield <zjhredfield@icloud.com>
2024-09-25 12:08:38 -0500

    updates to contact-app

--- contact-app.js
+++ contact-app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(helmet()); //Security 
 
 //MongoDB connection 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected!'))
     .catch(err => console.log(err));
 
